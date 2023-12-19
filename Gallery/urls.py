cc4f38e84903fbd1369992d8067c3f3acd8f7246
PatrikWithJustAK <patrik.gammon@gmail.com>
2023-12-19 14:11:39 -0500

    added delete view which takes in primary key. Dashboard updated to show Delete button on each image using template language. This passes a DELETE request to the endpoiint with a CSRF token

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import add_artpieceview, galleryindexview
+from .views import add_artpieceview, galleryindexview, deleteartpiece_view
 urlpatterns = [
     path("", add_artpieceview, name="add_artpiece"),
     path("dashboard/", galleryindexview, name="dashboard"),
+    path("deleteart/<int:pk>", deleteartpiece_view, name ="delete_artpiece"),
 ]
--- Gallery/views.py
+++ Gallery/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ArtPieceForm
 from .models import ArtPiece
-
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -17,8 +16,18 @@ def add_artpieceview(request):
         form = ArtPieceForm()
 
     return render(request, 'add-artpiece.html', {'form': form})
+
+@login_required
+def deleteartpiece_view(request, pk):
+    if request.method == 'DELETE':
+        artpiece = get_object_or_404(ArtPiece, id=pk, uploader=request.user)
+        artpiece.delete()
+        return redirect('dashboard')  # Redirect to gallery dashboard on delete
+    else:
+        return redirect('dashboard') 
+    
 def galleryindexview(request):
-    images = ArtPiece.objects.filter(uploader=request.user)
-    gallery = images.order_by('-created_at')
-    context = {"gallery" : gallery}
+    images = ArtPiece.objects.filter(uploader=request.user) #filter all pieces the user has created
+    gallery = images.order_by('-created_at') #filter by newest (this may be worth altering default in models.py)
+    context = {"gallery" : gallery} #The template has access to the gallery as an array of ArtPieces
     return render(request, 'dashboard.html', context)
